ab2a0b37908856f9cdab8d51ac361d219ef12964
Vlad Test <vladkretovsky@yandex.ru>
2024-07-10 14:08:29 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int ticetСost = 35839;
+        int miles = ticetСost / 20;
+
+        System.out.println(miles);
+
+    }
+}
